e4a480ad0b8c840274d4fe43fe8c9dbfd825b95f
balasaravanan-dhanwin-basil <145331692+balasaravanandhanwinbasil@users.noreply.github.com>
2025-04-27 21:14:20 +0800

    Add files via upload

---
+++ station1/station1.py
@@ -0,0 +1,115 @@
+import pyhula
+import time
+import cv2
+import numpy as np
+from hula_video import hula_video
+from tflite_detector import tflite_detector
+import threading
+
+uapi = pyhula.UserApi()
+
+stop_thread = False
+def blockheight():
+    global groundheight
+    global stop_thread
+
+    while not stop_thread:
+        blockheight = groundheight - uapi.get_plane_distance()
+        print(f"""
+              Height of object: {blockheight}
+              Height of Object from drone: {uapi.get_plane_distance()}
+        """)
+        time.sleep(0.01)
+
+def detect_blue(frame):
+    # Define HSV range for blue color
+    Lblue= np.array([0, 100, 100])
+    Ublue = np.array([140, 255, 255])
+    
+    # Convert frame to HSV color space
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # Create a mask for red color
+    mask_blue = cv2.inRange(hsv, Lblue, Ublue)
+    
+    # Find contours in the mask
+    contours, _ = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    if contours:
+        # Find the largest contour
+        largest_contour = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        center_x = x + w // 2
+        center_y = y + h // 2
+        
+        # Draw the detected contour and center
+        cv2.circle(frame, (center_x, center_y), 5, (0, 255, 0), -1)  # Green dot at center
+        
+        return center_x, center_y, frame
+    else:
+        return None, None, frame
+
+if not uapi.connect():
+    print('connect error')
+else:
+    print("connected")
+
+    uapi.Plane_cmd_camera_angle(2,90)
+    video = hula_video(hula_api=uapi,display=False)
+    detector = tflite_detector(model="model3.tflite",label="label.txt")
+    video.video_mode_on()
+
+    uapi.single_fly_takeoff()
+    i = 0
+    result = uapi.single_fly_recognition_Qrcode(0,0)
+    while i < 10 and result['result'] == False:
+        time.sleep(0.1)
+        i += 1
+        result = uapi.single_fly_recognition_Qrcode(mode = 0, qr_id = 0)
+
+    if result['result'] == True:
+        uapi.single_fly_straight_flight(x = result['x'], y = result['y'], z = 0)
+
+    #move forward and measure height of first box
+    groundheight = uapi.get_plane_distance()
+    
+    uapi.single_fly_forward(distance=90) #adjust
+
+    height_thread = threading.Thread(target=blockheight)
+    height_thread.start()
+
+    time.sleep(0.1)
+    uapi.single_fly_forward(distance=5)
+
+    stop_thread = True
+    
+    print("PHASE 1 COMPLETE")
+    time.sleep(2)
+
+    #get past the 2nd block
+    uapi.single_fly_forward(distance=75)
+
+    print("PHASE 2 COMPLETE")
+
+    #detect IMDA logo
+    time.sleep(3)
+    needgodown = True
+    for i in range(50):
+        frame = video.get_video()
+        object_found, frame = detector.detect(frame)
+        if not object_found is None:
+            print(F"Found object: {object_found}")
+            uapi.single_fly_lamplight(134,40,158,2,1)
+            needgodown = False
+            uapi.single_fly_touchdown()
+            break
+        else:
+            uapi.single_fly_forward(distance = 2.5)
+        cv2.imshow("Detection", frame)
+        cv2.waitKey(1)
+        time.sleep(0.1)
+    cv2.destroyAllWindows()
+    video.close()
+    print("PHASE 3 COMPLETE")
+    if needgodown:
+        uapi.single_fly_touchdown()
---
+++ station2/station2.py
@@ -0,0 +1,93 @@
+import pyhula
+import time
+from hula_video import hula_video
+from tflite_detector import tflite_detector
+import cv2
+import numpy as np
+import threading
+
+
+uapi = pyhula.UserApi()
+
+rotating = 0
+
+
+def rotate_half():
+    while rotating:
+        uapi.single_fly_radius_around(radius=40)
+
+def qrCodeAlign():
+    i = 0
+    result = uapi.single_fly_recognition_Qrcode(0,0)
+    while i < 10 and result['result'] == False:
+        time.sleep(0.1)
+        i += 1
+        result = uapi.single_fly_recognition_Qrcode(mode = 0, qr_id = 0)
+
+    if result['result'] == True:
+        uapi.single_fly_straight_flight(x = result['x'], y = result['y'], z = 0)
+
+if not uapi.connect():
+    print('connect error')
+else:
+    print("connected")
+
+    video = hula_video(hula_api=uapi,display=False)
+    detector = tflite_detector(model="model3.tflite",label="label.txt")
+    video.video_mode_on()
+    
+    uapi.single_fly_takeoff()
+    uapi.Plane_cmd_camera_angle(0,0)
+
+    qrCodeAlign()
+ 
+    # go back down a bit after takeoff
+    uapi.single_fly_down(height=50)
+
+    uapi.single_fly_forward(distance=45)
+    start_timer = time.time()
+    uapi.single_fly_radius_around(radius=50)
+    end_timer = time.time() #1
+    looptime = end_timer - start_timer
+    halflooptime = 0.5 * looptime
+    uapi.single_fly_radius_around(radius=50)
+
+    rotating = True
+    half_thread = threading.Thread(target = rotate_half)
+    half_thread.start()
+    print(halflooptime)
+    time.sleep(halflooptime)
+    rotating = False
+
+    qrCodeAlign()
+
+    for i in range(13):
+        frame = video.get_video()
+        object_found, frame = detector.detect(frame)
+        if not object_found is None:
+            print(F"Found object: {object_found}")
+            uapi.single_fly_lamplight(134,40,158,2,1)
+            needgodown = False
+        cv2.imshow("Detection", frame)
+        cv2.waitKey(1)
+        time.sleep(0.2)
+        uapi.single_fly_up(height=10)
+    cv2.destroyAllWindows()
+    video.close()
+    
+    uapi.single_fly_up(height = 110) 
+    uapi.single_fly_forward(distance=25) 
+    uapi.Plane_cmd_camera_angle(2,90)
+
+    i = 0
+    result = uapi.single_fly_recognition_Qrcode(0,0)
+    while i < 10 and result['result'] == False:
+        time.sleep(0.1)
+        i += 1
+        result = uapi.single_fly_recognition_Qrcode(mode = 0, qr_id = 0)
+
+    if result['result'] == True:
+        uapi.single_fly_straight_flight(x = result['x'], y = result['y'], z = 0)
+        uapi.single_fly_touchdown(led = {'r':255,'g':0,'b':0,'mode':1})
+    else:
+        uapi.single_fly_touchdown()  
