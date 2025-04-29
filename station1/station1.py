5444f7b1cb9ea11475f5579a35bab6329c4ee1c1
balasaravanan-dhanwin-basil <145331692+balasaravanandhanwinbasil@users.noreply.github.com>
2025-04-29 15:13:23 +0800

    Update station1.py
    Adding the aligning for the blue box

--- station1/station1.py
+++ station1/station1.py
@@ -48,7 +48,32 @@ def detect_blue(frame):
         return center_x, center_y, frame
     else:
         return None, None, frame
-
+def bluealign():
+       for i in range(50):
+        frame = video.get_video()
+        center_x, center_y, frame = detect_blue(frame)
+        print(center_x,center_y)
+        if not center_x == None:
+            while 50 < center_x and center_x > 10 and 50 < center_y and center_y < 10 :        
+                if center_x > 640:
+                    uapi.single_fly_right(20)
+                else:
+                    if center_x < 640:
+                        uapi.single_fly_left(20)
+                if center_y > 360:
+                    uapi.single_fly_back(20)
+                else:
+                    if center_y < 360:
+                        uapi.single_fly_forward(20)
+                cv2.imshow("Detection", frame)
+                cv2.waitKey(1)
+                print(center_x, center_y)
+                time.sleep(0.1)
+        time.sleep(0.1)
+    uapi.single_fly_forward(50)
+    cv2.destroyAllWindows()
+    video.stoprecording()
+    video.close()
 if not uapi.connect():
     print('connect error')
 else:
